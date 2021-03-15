db25d18e9d9bd9e539857d3c0dc907ce4dcd0f38
Antanas Arcimavicius <antanas.arcimavicius@505hq.com>
2021-03-15 18:25:19 +0200

    added home.hmtl

--- myblog/mainsite/urls.py
+++ myblog/mainsite/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from mainsite.views import HomeView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="maintenance_view"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
