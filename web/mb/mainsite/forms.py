48c8b29cbb00a375e5263e23fe12204e9eb3e43c
Antanas Arcimavicius <antanas.arcimavicius@505hq.com>
2021-05-03 13:18:34 +0300

    changed base design

--- web/mb/mainsite/forms.py
+++ web/mb/mainsite/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True, label="From email")
-    subject = forms.CharField(required=True, label="Subject")
+    from_email = forms.EmailField(
+        required=True, label="From email", widget=forms.TextInput(attrs={"placeholder": "From Email"})
+    )
+    subject = forms.CharField(required=True, label="Subject", widget=forms.TextInput(attrs={"placeholder": "Subject"}))
     message = forms.CharField(widget=forms.Textarea, required=True, label="Message")
