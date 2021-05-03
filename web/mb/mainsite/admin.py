2155aa6edd6b1de984a9604388e5ebb5c20b9ab9
Antanas Arcimavicius <antanas.arcimavicius@505hq.com>
2021-05-03 15:34:19 +0300

    added Skill, Experience, Education and Resume Models

--- web/mb/mainsite/admin.py
+++ web/mb/mainsite/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from .models import Education, Resume, Skill, WorkExperience
+
 # Register your models here.
+
+
+class SkillInline(admin.TabularInline):
+    model = Skill
+
+    def get_extra(self, request, obj=None, **kwargs):
+        return 0
+
+
+class WorkExperienceInline(admin.TabularInline):
+    model = WorkExperience
+
+    def get_extra(self, request, obj=None, **kwargs):
+        return 0
+
+
+class EducationInline(admin.TabularInline):
+    model = Education
+
+    def get_extra(self, request, obj=None, **kwargs):
+        return 0
+
+
+@admin.register(Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    inlines = [
+        SkillInline,
+        WorkExperienceInline,
+        EducationInline,
+    ]
--- web/mb/mainsite/models.py
+++ web/mb/mainsite/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Resume(models.Model):
+    owner = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to="mainsite", height_field=None, width_field=None, blank=True)
+    role = models.CharField(max_length=255)
+    about = models.TextField()
+
+    def __str__(self):
+        return f"{self.owner} CV"
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    portfolio = models.ForeignKey("mainsite.Resume", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} skill"
+
+
+class WorkExperience(models.Model):
+    company = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    employment_type = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+    portfolio = models.ForeignKey("mainsite.Resume", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.role} in {self.company}"
+
+
+class Education(models.Model):
+    school = models.CharField(max_length=255)
+    field_of_study = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+    portfolio = models.ForeignKey("mainsite.Resume", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.school}, {self.degree}, {self.field_of_study}"
