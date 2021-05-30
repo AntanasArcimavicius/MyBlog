4e2ba740c5c48d9fece8e0f42f3ea6abaefa4196
antanas_505hq <antanas.arcimavicius@505hq.com>
2021-05-30 16:38:19 +0300

    about section draft

---
+++ web/mb/static/js/functions.js
@@ -0,0 +1,19 @@
+const body = document.body
+const nav = document.querySelector("#myNav");
+const navTop = document.querySelector(".nav-top")
+const aboutSection = document.querySelector(".about-section")
+
+function openNav() {
+    nav.classList.toggle("opened");
+    navTop.classList.toggle("clearfix")
+}
+
+function scrollToAbout() {
+    aboutSection.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+
+body.addEventListener("click", function(event) {
+    if (!nav.contains(event.target)) {
+        nav.className = "navbar";
+    }
+} );
