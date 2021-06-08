35f11145385f549b783e8d8ec25421b99e4ff255
antanas_505hq <antanas.arcimavicius@505hq.com>
2021-06-08 18:10:29 +0300

    moved script from email_form to functions.js, fixed popup display on mobile

--- web/mb/static/js/functions.js
+++ web/mb/static/js/functions.js
@@ -1,7 +1,8 @@
-const body = document.body
 const nav = document.querySelector("#myNav");
 const navTop = document.querySelector(".nav-top")
 const aboutSection = document.querySelector(".about-section")
+const email_modal = document.querySelector(".email-modal")
+const profile_img = document.querySelector("#profile-photo")
 
 function openNav() {
     nav.classList.toggle("opened");
@@ -13,8 +14,19 @@ function scrollToAbout() {
     nav.className = "navbar";
 }
 
-body.addEventListener("click", function(event) {
+document.body.addEventListener("click", function(event) {
     if (!nav.contains(event.target)) {
         nav.className = "navbar";
     }
 } );
+
+document.querySelector(".open-email").addEventListener("click", function() {
+    email_modal.style.display = "flex";
+    profile_img.style.display = "none";
+    nav.className = "navbar";
+});
+
+document.querySelector(".close").addEventListener("click", function() {
+    email_modal.style.display = "none";
+    profile_img.style.display = "inline-block";
+});
