9cbc87e6ad24b4fdaf0fd9dbcf8b393758eeb9f9
anthony <arcimavic@gmail.com>
2021-07-12 18:25:06 +0300

    minor fix for email popup

--- web/mb/static/js/functions.js
+++ web/mb/static/js/functions.js
@@ -4,6 +4,7 @@ const aboutSection = document.querySelector(".about-section");
 const emailModal = document.querySelector(".email-modal");
 const profileImg = document.querySelector("#profile-photo");
 const closeButton = document.querySelector(".close");
+const header = document.querySelector(".header")
 
 function openNav() {
     nav.classList.toggle("opened");
@@ -18,6 +19,7 @@ function scrollToAbout() {
 document.body.addEventListener("click", function(event) {
     if (!nav.contains(event.target)) {
         nav.className = "navbar";
+        navTop.classList.remove("clearfix");
     }
 } );
 
@@ -25,12 +27,13 @@ document.querySelector(".open-email").addEventListener("click", function() {
     emailModal.style.display = "flex";
     profileImg.style.display = "none";
     nav.className = "navbar";
+    header.style.zIndex = 0;
     closeButton.style.zIndex = 30;
-    console.log("working")
 });
 
 closeButton.addEventListener("click", function() {
     emailModal.style.display = "none";
     profileImg.style.display = "inline-block";
+    header.style.zIndex = 10;
     closeButton.style.zIndex = 0;
 });
